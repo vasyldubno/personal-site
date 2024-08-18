d95f906af6a6e4c859d9e3e779f871f160bc897a
Vasyl Dubno <vasyldubno@gmail.com>
2024-08-18 18:08:52 +0300

    update configure files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { 
+        protocol: 'https', 
+        hostname: 'res.cloudinary.com'
+      }
+    ]
+    // loader: 'cloudinary',
+    // path: 'https://res.cloudinary.com/dtkchspyx/image/upload',
+  }
+};
 
 export default nextConfig;
